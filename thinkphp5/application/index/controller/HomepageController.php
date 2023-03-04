3bf353ba4673f5effa76853b9045d172bf4585d7
gb <13927029761@163.com>
2023-03-05 07:01:42 +0800

    修改

--- thinkphp5/application/index/controller/HomepageController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\index\controller;
-use think\Controller;
-class HomepageController extends Controller{
-    public function index(){
-        return $this->fetch();
-    }
-}
--- thinkphp5/application/index/controller/SystemController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\index\controller;
-use think\Controller;
-class SystemController extends Controller{
-    public function index(){
-        return $this->fetch();
-    }
-}
