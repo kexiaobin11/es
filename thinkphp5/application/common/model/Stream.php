3f85f4f8ed45bde31cd12a0956390ba24efc3805
gb <13927029761@163.com>
2023-03-08 19:27:47 +0800

    15

---
+++ thinkphp5/application/common/model/Stream.php
@@ -0,0 +1,51 @@
+<?php
+namespace app\common\model;
+use think\Model;
+
+class Stream extends Model{
+
+    /**
+     * 输出性别的属性
+     * @return string 0支出，1收入
+     * 
+     */
+    public function getInandExAttr($value)
+    {
+        $status = array('0'=>'支','1'=>'收');
+
+        $inandex = $status[$value];
+
+        if (isset($inandex))
+
+        {
+            return $inandex;
+
+        } else {
+
+            return $status[0];
+
+        }
+
+    }
+
+    // 收入类型
+  public function Income()
+  {
+    return $this->belongsTo('Income');
+  }
+
+       // 支出类型
+    public function Pay()
+    {
+
+        return $this->belongsTo('Pay');
+
+    }
+
+    public function Account()
+    {
+
+        return $this->belongsTo('Account');
+
+    }
+}
--- thinkphp5/application/common/model/User.php
+++ thinkphp5/application/common/model/User.php
@@ -5,31 +5,64 @@ use think\Request;
 class User extends Model{
     static public function login($username,$password){
         $map = array('username' => $username);
+
         $User = self::get($map);
-        if(!is_null($User)){
-            if($User->checkPassword($password)){
-                session('teacherId', $User->getData('id'));
+
+        if(!is_null($User))
+        {
+            if($User->checkPassword($password))
+            {
+
+                session('userId', $User->getData('id'));
+
                 return true;
             }
         }
     }
+
     public function checkPassword($password){
-        if($this->getData('password') === $this::encryptPassword($password)){
+
+        if($this->getData('password') === $this::encryptPassword($password))
+        {
             return true;
-        }else{
+        }
+        else
+        {
             return false;
         }
     }
-    static public function encryptPassword($password){
-        // 实际的过程中，我还还可以借助其它字符串算法，来实现不同的加密。
-        if(!is_string($password)){
+    static public function encryptPassword($password)
+    {
+        
+        if(!is_string($password))
+        {
            throw new \RuntimeException("传入变量类型非字符串，错误码2", 2);
         }
+
       return sha1(md5($password) . 'mengyunzhi');
-    }
-        static public function logOut(){
+     }
+
+        static public function logOut()
+        {
             session('userId',null);
+
             return true;
+        }
+
+        static public function isLogin()
+        {
+            $userId = session('userId');
+            
+            if(isset($userId)) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+
         }
 
 }
